eba476f56c41403021ae5bcd450c764edd33d49d
LieuDinh <lhd230624@gmail.com>
2025-03-06 01:48:54 +0700

    init source FE

---
+++ frontend/src/layouts/AdminLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export const AdminLayout = () => {
+  return (
+    <div>
+      <h1>Admin Layout</h1>
+      <Outlet />
+    </div>
+  );
+};
+
---
+++ frontend/src/pages/statistic/Statistic.tsx
@@ -0,0 +1,7 @@
+export const Statistic = () => {
+    return (
+        <div>
+            <h1>Statistic</h1>
+        </div>
+    )
+}
