85bc1a0fa85ff1de58c0a45483be6f40d12f2674
Lê Nguyễn Gia Hưng <ghung21112004@gmail.com>
2025-03-15 17:07:48 +0700

    setup models

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -5,10 +5,6 @@ class Notification(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
     user_id = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    device_id = models.IntegerField(null=True, blank=True)
-    device_name = models.CharField(max_length=255, null=True, blank=True)
-    action = models.CharField(max_length=10, choices=[('on', 'On'), ('off', 'Off'), ('open', 'Open'), ('close', 'Close')], null=True, blank=True)
-    user_name = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return f"{self.device_name} - {self.action} by {self.user_name} at {self.timestamp}"
