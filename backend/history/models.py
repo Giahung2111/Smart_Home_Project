9e99f25dbe76b7ae0eab79f2b36bd74eeb8aee1f
Lê Nguyễn Gia Hưng <ghung21112004@gmail.com>
2025-03-15 18:16:24 +0700

    setup models

--- backend/history/models.py
+++ backend/history/models.py
@@ -5,6 +5,7 @@ class History(models.Model):
     status = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
     device_id = models.ForeignKey("devices.Device", on_delete=models.CASCADE)
-
+    userid = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    
     def __str__(self):
         return f"{self.device_id} - {self.status} at {self.timestamp}"
