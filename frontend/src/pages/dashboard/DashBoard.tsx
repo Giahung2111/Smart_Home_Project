85b96198c2f7d114bc5d69a50c94e78bc7e7369a
Lhdinh2306 <166101614+Lhdinh2306@users.noreply.github.com>
2025-05-06 23:24:49 +0700

    Fix bug in dashboard page

--- frontend/src/pages/dashboard/DashBoard.tsx
+++ frontend/src/pages/dashboard/DashBoard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, Switch } from "antd";
+import { Avatar, Card, message, Switch } from "antd";
 import {
   faLightbulb,
   faDoorClosed,
@@ -16,21 +16,22 @@ import { DashboardAPI } from "../../services/dashboard/dashboardAPI";
 import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
-  const [light, setLight] = useState({id: 0, name: '', status: false});
-  const [fan, setFan] = useState({id: 0, name: '', status: false});
-  const [door, setDoor] = useState({id: 0, name: '', status: false});
+  const [light, setLight] = useState({ id: 0, name: '', status: false });
+  const [fan, setFan] = useState({ id: 0, name: '', status: false });
+  const [door, setDoor] = useState({ id: 0, name: '', status: false });
   const memberUrl = DashboardAPI.getAllMembersUrl;
   const livingRoomUrl = DashboardAPI.getAllDevicesInLivingRoomUrl;
   const deviceUrl = DashboardAPI.updateDeviceUrl;
   const [users, setUsers] = useState<IDashboardUserProps[]>([]);
+  const [messageApi, contextHolder] = message.useMessage();
 
   const getAllUsers = async () => {
     try {
       const response = await axios.get(memberUrl);
-      
-      if(response.data.status === 200) {
+
+      if (response.data.status === 200) {
         const data = response.data.data;
-        const usersData = data.map((user : IDashboardUserResponseProps) => ({
+        const usersData = data.map((user: IDashboardUserResponseProps) => ({
           name: user.FullName,
           role: user.Role,
           color: user.Avatar,
@@ -47,11 +48,11 @@ export const Dashboard = () => {
   const getAllDevicesInLivingRoom = async () => {
     const response = await axios.get(livingRoomUrl);
     const data = response.data.data['living room']['devices'];
-    
+
     console.log(data);
 
     for (let i = 0; i < data.length; i++) {
-      if(data[i].device_name == "fan") {
+      if (data[i].device_name == "fan") {
         setFan({
           id: data[i].id,
           name: data[i].device_name,
@@ -59,7 +60,7 @@ export const Dashboard = () => {
         })
       }
 
-      if(data[i].device_name == "door") {
+      if (data[i].device_name == "door") {
         setDoor({
           id: data[i].id,
           name: data[i].device_name,
@@ -67,7 +68,7 @@ export const Dashboard = () => {
         });
       }
 
-      if(data[i].device_name == "light 4") {
+      if (data[i].device_name == "light 4") {
         setLight({
           id: data[i].id,
           name: data[i].device_name,
@@ -77,15 +78,22 @@ export const Dashboard = () => {
     }
   }
 
-  const handleUpdateDeviceStatus = async (id: number, status : boolean) => {
-    const currentUser = JSON.parse(localStorage.getItem('user') as string);
-    console.log("Current user: ", currentUser);
-    const response = await axios.patch(`${deviceUrl}${id}/`, {
-      status : status,
-      userID : currentUser.id,
-    });
-
-    console.log(response.data)
+  const handleUpdateDeviceStatus = async (id: number, status: boolean) => {
+    try {
+      const currentUser = JSON.parse(localStorage.getItem('user') as string);
+      console.log("Current user: ", currentUser);
+      const response = await axios.patch(`${deviceUrl}${id}/`, {
+        status: status,
+        userID: currentUser.id,
+      });
+      console.log(response.data)
+      if (response?.data?.data?.id) {
+        return true
+      }
+      return false
+    } catch (error) {
+      return false
+    }
   }
 
   useEffect(() => {
@@ -94,51 +102,65 @@ export const Dashboard = () => {
   }, [])
 
   return (
-    <div 
+    <div
       className="p-6 space-y-6"
-      style={{backgroundColor: 'var(--background-color)', color: 'var(--text-color)'}}>
+      style={{ backgroundColor: 'var(--background-color)', color: 'var(--text-color)' }}>
       {/* Member Section */}
       <section>
         <h2 className="text-xl font-semibold mb-4">Member</h2>
-        <div 
-        className="bg-gray-100 p-4 rounded-xl flex gap-4"
-        style={{backgroundColor: 'var(--background-color)', color: 'var(--text-color)'}}>
+        <div
+          className="bg-gray-100 p-4 rounded-xl flex gap-4"
+          style={{ backgroundColor: 'var(--background-color)', color: 'var(--text-color)' }}>
           {users.map((member, index) => (
             <div key={index} className="text-center">
               <Avatar size="large" style={{ backgroundColor: member.color }}>
                 {member.initials}
               </Avatar>
               <p className="font-semibold">{member.name}</p>
-              <p className="text-gray-500 text-sm" style={{color: 'var(--text-color)'}}>{member.role}</p>
+              <p className="text-gray-500 text-sm" style={{ color: 'var(--text-color)' }}>{member.role}</p>
             </div>
           ))}
         </div>
       </section>
 
       <section>
-        <h2 
+        <h2
           className="text-xl font-semibold mb-4">Devices</h2>
-        <div 
-        className="bg-gray-100 p-4 rounded-xl"
-        style={{backgroundColor: 'var(--background-color)', color: 'var(--text-color)'}}>
+        <div
+          className="bg-gray-100 p-4 rounded-xl"
+          style={{ backgroundColor: 'var(--background-color)', color: 'var(--text-color)' }}>
           <Row gutter={[16, 16]} justify="center">
             <Col span={8}>
               <Card className="w-full text-center p-4 shadow-md">
                 <Row gutter={[16, 16]} justify="center">
                   <Col span={8}>
                     <span className="text-2xl">
-                      <FontAwesomeIcon icon={light ? faLightbulb : regularLightbulb} />
+                    <FontAwesomeIcon icon={light.status ? faLightbulb : regularLightbulb} />
                     </span>
                   </Col>
                   <Col span={8}>
-                    <p className="font-semibold">{light ? "ON" : "OFF"}</p>
+                    <p className="font-semibold">{light.status ? "ON" : "OFF"}</p>
                   </Col>
                   <Col span={8}>
-                    <Switch 
-                      checked={light.status} 
-                      onChange={() => {
-                        setLight({...light, status: !light.status})
-                        handleUpdateDeviceStatus(light.id, !light.status);
+                    <Switch
+                      checked={light.status}
+                      onChange={async () => {
+                        const updateStatus = !light.status;
+                        const res = await handleUpdateDeviceStatus(light.id, updateStatus);
+
+                        if (res) {
+                          setLight({ ...light, status: updateStatus })
+                          messageApi.open({
+                            type: 'success',
+                            content: `Turned ${updateStatus ? "ON" : "OFF"} the Light`,
+                          });
+                        }
+                        else {
+                          messageApi.open({
+                            type: 'error',
+                            content: `Failed to turn ${updateStatus ? "ON" : "OFF"} the Light`,
+                          });
+                        }
                       }}
                     />
                   </Col>
@@ -155,15 +177,29 @@ export const Dashboard = () => {
                     </span>
                   </Col>
                   <Col span={8}>
-                    <p className="font-semibold">{fan ? "ON" : "OFF"}</p>
+                    <p className="font-semibold">{fan.status ? "ON" : "OFF"}</p>
                   </Col>
                   <Col span={8}>
-                    <Switch 
-                    checked={fan.status} 
-                    onChange={() => {
-                      setFan({...fan, status: !fan.status})
-                      handleUpdateDeviceStatus(fan.id, !fan.status);
-                    }}
+                    {/* <Switch
+                      checked={fan.status}
+                      onChange={() => {
+                        setFan({ ...fan, status: !fan.status })
+                        handleUpdateDeviceStatus(fan.id, !fan.status);
+                      }}
+                    /> */}
+                    <Switch
+                      checked={fan.status}
+                      onChange={async () => {
+                        const updateStatus = !fan.status;
+                        const res = await handleUpdateDeviceStatus(fan.id, updateStatus);
+
+                        if (res) {
+                          setFan({ ...fan, status: updateStatus });
+                          messageApi.success(`Fan turned ${updateStatus ? "ON" : "OFF"}`);
+                        } else {
+                          messageApi.error(`Failed to update fan status`);
+                        }
+                      }}
                     />
                   </Col>
                 </Row>
@@ -175,19 +211,33 @@ export const Dashboard = () => {
                 <Row gutter={[16, 16]} justify="center">
                   <Col span={8}>
                     <span className="text-2xl">
-                      <FontAwesomeIcon icon={door ? faDoorOpen : faDoorClosed} />
+                      <FontAwesomeIcon icon={door.status ? faDoorOpen : faDoorClosed} />
                     </span>
                   </Col>
                   <Col span={8}>
-                    <p className="font-semibold">{door ? "OPEN" : "CLOSE"}</p>
+                    <p className="font-semibold">{door.status ? "OPEN" : "CLOSE"}</p>
                   </Col>
                   <Col span={8}>
-                    <Switch 
-                    checked={door.status} 
-                    onChange={() => {
-                      setDoor({...door, status: !door.status})
-                      handleUpdateDeviceStatus(door.id, !door.status);
-                    }}
+                    {/* <Switch
+                      checked={door.status}
+                      onChange={() => {
+                        setDoor({ ...door, status: !door.status })
+                        handleUpdateDeviceStatus(door.id, !door.status);
+                      }}
+                    /> */}
+                    <Switch
+                      checked={door.status}
+                      onChange={async () => {
+                        const updateStatus = !door.status;
+                        const res = await handleUpdateDeviceStatus(door.id, updateStatus);
+
+                        if (res) {
+                          setDoor({ ...door, status: updateStatus });
+                          messageApi.success(`Door ${updateStatus ? "opened" : "closed"}`);
+                        } else {
+                          messageApi.error(`Failed to update door status`);
+                        }
+                      }}
                     />
                   </Col>
                 </Row>
@@ -196,6 +246,7 @@ export const Dashboard = () => {
           </Row>
         </div>
       </section>
+      {contextHolder}
     </div>
   );
 };
